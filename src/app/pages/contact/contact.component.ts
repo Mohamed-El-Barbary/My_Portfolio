6e56a3c2e648865e7ee49626aabd19387cbe7a48
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2025-11-24 20:49:14 +0200

    feat(contact): add contact component HTML template

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MainTitleComponent } from "../../shared/components/main-title/main-title.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MainTitleComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
